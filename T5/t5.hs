6ae55984550dfb3b2022c87343c9a35238d5136f
Lucas <lima.lucasdeoliveira@gmail.com>
2015-09-04 20:03:37 -0300

    Update

--- T5/t5.hs
+++ T5/t5.hs
@@ -31,10 +31,62 @@ combine _[] = []
 combine (n:ns) (x:xs) = (n, x) : combine ns xs
 
 ----------------- EXERCÍCIO 7 -----------------
---numera :: [String] -> [(Int, String)]
---mumera :: [] = []
---numera x = 
+numera :: [String] -> [(Int, String)]
+numera x = zip (auxNumera (length x)) x
 
 auxNumera :: Int -> [Int]
 auxNumera 0 = []
-auxNumera n = auxNumera (n-1) ++ n
+auxNumera n = auxNumera (n-1) ++ [n]
+
+----------------- EXERCÍCIO 8 -----------------
+{-
+	A) 
+	> [ (x,y) | x <- [1..5], even x, y <- [(x + 1)..6], odd y]
+	[(2,3),(2,5),(4,5)]
+
+	Este comando retorna uma lista com tuplas de números inteiros, a partir
+	de duas listas, a primeira contém os valores de 1 a 5 e são selecionados
+	apenas os valores pares, que são unidos com os valores ímpares da
+	segunda lista a partir do número selecionado +1 até 6.
+
+	B) 
+	> [ a ++ b | a <- ["lazy", "big"], b <- ["frog", "dog"]]
+	["lazyfrog","lazydog","bigfrog","bigdog"]
+
+	Este comando retorna uma lista de strings a partir de duas outras listas
+	com a união de cada elemento da primeira lista com todos os outros da
+	segunda.
+
+	C)
+	> concat [ [a, '-'] | a <- "paralelepipedo", a `elem` "aeiou"]
+	"a-a-e-e-i-e-o-"
+
+	Esta expressão retorna uma lista de strings contendo apenas as vogais 
+	da palavra "paralelepipedo" seguidas de um hífen '-'. Esta lista é 
+	concatenada pela função "concat", resultadno em uma só string, ou lista
+	de caracteres.
+
+-}
+
+----------------- EXERCÍCIO 9 -----------------
+crossProdutct :: [a] -> [b] -> [(a, b)]
+crossProdutct _[] = []
+crossProdutct []_ = []
+crossProdutct (x:xs) ys = pairWithAll x ys ++ crossProdutct xs ys
+
+pairWithAll :: a -> [b] -> [(a, b)]
+pairWithAll _[] = []
+pairWithAll x (y:ys) = (x, y) : pairWithAll x ys
+
+----------------- EXERCÍCIO 10 -----------------
+genRects :: Int -> (Int, Int) -> [(Float, Float, Float, Float)]
+genRects n xy = genRectsAux (n+1) (((fst xy)/1), ((snd xy)/1))
+
+genRectsAux :: Int -> (Float, Float) -> [(Float, Float, Float, Float)]
+genRectsAux 0_ = []
+genRectsAux n xy =
+	let 
+		largAlt = 5.5
+		x = fst xy
+		y = snd xy
+	in (x, y, largAlt, largAlt) : genRectsAux (n-1) (x+largAlt, y)

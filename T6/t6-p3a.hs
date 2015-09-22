50c35861e66e1dcc26382bee7c0160b26ba55191
Lucas <loliveira@inf.ufsm.br>
2015-09-22 03:50:57 -0300

    Update

--- T6/t6-p3a.hs
+++ T6/t6-p3a.hs
@@ -2,16 +2,16 @@ import Text.Printf
 
 -- Gera codigo-fonte de arquivo SVG 
 -- concatenando uma lista de retangulos e seus atributos de estilo
-writeCircles :: Float -> Float -> (Float,Float) -> [(String,Float)] -> String
-writeCircles w h center cs = 
+writeCircles :: Float -> Float -> Float -> [(String,Float)] -> String
+writeCircles w h xy cs = 
   printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
-      ++ (concatMap (writeCircle center cs)) ++ "</svg>"
+      ++ (concatMap (writeCircle xy) cs) ++ "</svg>"
 
 -- Gera circulo SVG 
 -- a partir de coordenadas+raio e de uma string com atributos de estilo
-writeCircle :: (Float,Float) -> (String,Float) -> String 
-writeCircle (x,y) (style,r) = 
-  printf "<circle cx='%.2f' cy='%.2f' r='%.2f' style='%s' />\n" x y r style
+writeCircle :: Float -> (String,Float) -> String 
+writeCircle xy (style,r) = 
+  printf "<circle cx='%.2f' cy='%.2f' r='%.2f' style='%s' />\n" xy xy r style
 
 -- Gera a o raio de cada circulo
 -- Baseado no raio máximo e quantidade de circulos
@@ -29,10 +29,10 @@ elemStyle r qnt = ["fill:hsl(" ++ show ((360/qnt)*c) ++ ", 100%, 50%); fill-opac
 main :: IO ()
 main = do
   let
-    maxRad = 1000;    -- Raio máximo, do ultimo circulo exterior
-    qntCircle = 10;   -- Número de circulos sobrepostos
+    maxRad = 360;       -- Raio máximo, do ultimo circulo exterior
+    qntCircle = 360;    -- Número de circulos sobrepostos
 
     style = elemStyle maxRad qntCircle
-    circles = zip style elemRadius maxRad qntCircle
+    circles = zip style (reverse (elemRadius maxRad qntCircle))
     -- Exemplo de elemento contido em circles = [("fill:hsl...",100.0)]
-  writeFile "colors-p3a.svg" $ writeCircles (maxRad*2) (maxRad*2) (maxRad,maxRad) circles
+  writeFile "colors-p3a.svg" $ writeCircles (maxRad*2) (maxRad*2) maxRad circles

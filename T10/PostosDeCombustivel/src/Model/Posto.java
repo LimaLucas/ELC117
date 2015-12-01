188309e69b933d86011bdc3cae5e5119fb838cbb
LimaLucas <lima.lucasdeoliveira@gmail.com>
2015-12-01 17:01:12 -0200

    Criado: Projeto, pacotes MVC e classe Posto

---
+++ T10/PostosDeCombustivel/src/Model/Posto.java
@@ -0,0 +1,97 @@
+package Model;
+
+/**
+ *
+ * @author LucasLima
+ */
+public class Posto {
+    private int id;
+    private String cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+    private String bandeira;
+    private String endereco;
+    private String bairro;
+    private String imagem;
+    
+    public Posto(){}
+    
+    public Posto(int id, String cnpj, String razaoSocial, String nomeFantasia, String bandeira, String endereco, String bairro, String imagem){
+        this.id = id;
+        this.cnpj = cnpj;
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
+        this.bandeira = bandeira;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.imagem = imagem;
+    }
+    
+    // Métodos Getter e Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+    
+    
+}
